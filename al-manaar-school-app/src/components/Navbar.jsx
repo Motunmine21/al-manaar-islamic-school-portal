852da1825cc80cb5abbc3517fddd63f8018c9de5
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-02-23 14:46:58 -0800

    Update components for admin features

---
+++ al-manaar-school-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | 
+      <Link to="/about">About</Link> | 
+      <Link to="/academics">Academics</Link> | 
+      <Link to="/contact">Contact</Link> | 
+      <Link to="/admissions">Admissions</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
