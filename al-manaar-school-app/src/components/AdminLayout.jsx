85ae09d6dc38edea64eb3c779f77ec5e87ce4c1c
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-06-17 07:04:45 -0700

    Add the AdminRoutes.jsx file

--- al-manaar-school-app/src/components/AdminLayout.jsx
+++ al-manaar-school-app/src/components/AdminLayout.jsx
@@ -2,12 +2,16 @@ import AdminSidebar from "./AdminSidebar";
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen bg-gray-100">
+
+      {/* ✅ ONLY ONE SIDEBAR */}
       <AdminSidebar />
 
-      <div className="flex-1 p-6 bg-gray-100 min-h-screen">
+      {/* MAIN CONTENT */}
+      <div className="flex-1 p-6">
         {children}
       </div>
+
     </div>
   );
 }
