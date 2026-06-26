31b74f7e0dd7fdc3c8c2e4980dd18d8553eab97a
Motunmine21 <216323922+Motunmine21@users.noreply.github.com>
2026-06-26 15:45:55 -0700

    Update the StudentUtils file

--- al-manaar-school-app/src/components/Utils/StudentUtils.js
+++ al-manaar-school-app/src/components/Utils/StudentUtils.js
@@ -1,5 +1,5 @@
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase/firebase";
+import { db } from "../../firebase/firebase";
 
 /**
  * Generate the next Student ID
